a039d9088092264202eaf03716984b7f671258d5
Alex Koufatzis <4354310+akoufatzis@users.noreply.github.com>
2019-01-04 20:23:43 +0200

    Update gradle to 5.1 and various dependencies Add gradle plugin to check for dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.github.ben-manes.versions").version("0.20.0")
+}
 
 buildscript {
     repositories {
