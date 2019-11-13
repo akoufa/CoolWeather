8d1d82c93963849d8f819e8d86196f6aed75e589
Alexandros Koufatzis <4354310+akoufatzis@users.noreply.github.com>
2019-11-13 23:02:08 +0100

    Add externalId to PlaceEntity

--- app/src/main/java/com/akoufatzis/coolweather/data/database/entities/PlaceEntity.kt
+++ app/src/main/java/com/akoufatzis/coolweather/data/database/entities/PlaceEntity.kt
@@ -1,16 +1,23 @@
 package com.akoufatzis.coolweather.data.database.entities
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.akoufatzis.coolweather.domain.place.Place
 
-@Entity(tableName = "places")
+@Entity(
+    tableName = "places", indices = [Index(
+        value = ["name", "externalId"],
+        unique = true
+    )]
+)
 data class PlaceEntity(
-    var name: String
+    var name: String,
+    var externalId: String
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
 }
 
-fun fromPlace(place: Place) = PlaceEntity(place.name)
-fun PlaceEntity.toPlace() = Place(this.name)
+fun fromPlace(place: Place) = PlaceEntity(place.name, place.id)
+fun PlaceEntity.toPlace() = Place(this.name, this.externalId)
